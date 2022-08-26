3e879bbe525f2c56dd16930933d9bca85b535047
riandeoliveira <riandiasdeoliveira2001@gmail.com>
2022-08-26 11:54:12 -0300

    refactor: clean default structure

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,7 @@
-import Main from "templates/Main";
 import type { NextPage } from "next";
 
 const Home: NextPage = (): JSX.Element => {
-  return (
-    <>
-      <Main>
-        <h1>Hello, Rian Oliveira!</h1>
-        <h2>Have a nice coding ;)</h2>
-      </Main>
-    </>
-  );
+  return <h1>Hello World</h1>;
 };
 
 export default Home;
