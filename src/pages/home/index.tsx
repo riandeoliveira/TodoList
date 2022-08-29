5b9adb9fcd9b07b9ded4c1025f675710aa721bde
riandeoliveira <riandiasdeoliveira2001@gmail.com>
2022-08-28 23:39:45 -0300

    refactor: update Home pge

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,10 +1,23 @@
 import { Header } from "components/Header";
 import { TodoForm } from "components/TodoForm";
 import { TodoList } from "components/TodoList";
+import { TodoContext } from "contexts/TodoContext";
+import type { ITodo, ITodoContext } from "interfaces";
 import type { NextPage } from "next";
+import { useContext, useEffect } from "react";
 import styles from "./styles.module.scss";
 
 const Home: NextPage = (): JSX.Element => {
+  const { setTodoList } = useContext(TodoContext) as ITodoContext;
+
+  useEffect(() => {
+    const storageTodos: ITodo[] = JSON.parse(
+      localStorage.getItem("todos") || "[]"
+    );
+
+    setTodoList(storageTodos);
+  }, []);
+
   return (
     <div className={styles.layout}>
       <div className={styles.container}>
