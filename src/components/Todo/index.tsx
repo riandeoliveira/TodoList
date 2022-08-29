08593516fa56aac165207da22edf5e62c602c5e1
riandeoliveira <riandiasdeoliveira2001@gmail.com>
2022-08-28 23:36:56 -0300

    feat: update Todo component

--- src/components/Todo/index.tsx
+++ src/components/Todo/index.tsx
@@ -1,14 +1,25 @@
+import { TodoContext } from "contexts/TodoContext";
+import type { ITodo, ITodoContext } from "interfaces";
 import { FiEdit, FiInfo, HiOutlineTrash } from "libs/react-icons";
+import { useContext } from "react";
 import styles from "./styles.module.scss";
 
-interface TodoProps {
-  name: string;
-}
+interface TodoProps extends Omit<ITodo, "description"> {}
 
-export const Todo = ({ name }: TodoProps): JSX.Element => {
+export const Todo = ({ id, name, completed }: TodoProps): JSX.Element => {
+  const { completeTodo, removeTodo } = useContext(TodoContext) as ITodoContext;
+
   return (
-    <div className={`${styles.todo_container} ${styles.completed}`}>
-      <div className={styles.todo} tabIndex={0}>
+    <div
+      className={`${styles.todo_container} ${
+        completed ? styles.completed : ""
+      }`}
+    >
+      <div
+        className={styles.todo}
+        tabIndex={0}
+        onClick={() => completeTodo(id)}
+      >
         <span className={styles.name}>{name}</span>
       </div>
       <div className={styles.option_container}>
@@ -18,7 +29,12 @@ export const Todo = ({ name }: TodoProps): JSX.Element => {
         <button type="button" title="Editar tarefa" className={styles.button}>
           <FiEdit size={22} className={styles.icon} />
         </button>
-        <button type="button" title="Remover tarefa" className={styles.button}>
+        <button
+          type="button"
+          title="Remover tarefa"
+          className={styles.button}
+          onClick={() => removeTodo(id)}
+        >
           <HiOutlineTrash size={22} className={styles.icon} />
         </button>
       </div>
