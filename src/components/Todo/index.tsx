1d28a9465ddda696571159a872e0028899ac1f96
riandeoliveira <riandiasdeoliveira2001@gmail.com>
2022-08-29 14:21:46 -0300

    refactor: update Todo component

--- src/components/Todo/index.tsx
+++ src/components/Todo/index.tsx
@@ -1,3 +1,4 @@
+import { TodoModal } from "components/TodoModal";
 import { TodoContext } from "contexts/TodoContext";
 import type { ITodo, ITodoContext } from "interfaces";
 import { FiEdit, FiInfo, HiOutlineTrash } from "libs/react-icons";
@@ -8,44 +9,58 @@ import styles from "./styles.module.scss";
 interface TodoProps extends Omit<ITodo, "description"> {}
 
 export const Todo = ({ id, name, completed }: TodoProps): JSX.Element => {
-  const { completeTodo, removeTodo } = useContext(TodoContext) as ITodoContext;
+  const { completeTodo, removeTodo, handleOpenModal, setTodoId } = useContext(
+    TodoContext
+  ) as ITodoContext;
 
   const router: NextRouter = useRouter();
 
   return (
-    <div
-      className={`${styles.todo_container} ${
-        completed ? styles.completed : ""
-      }`}
-    >
+    <>
       <div
-        className={styles.todo}
-        tabIndex={0}
-        onClick={() => completeTodo(id)}
+        className={`${styles.todo_container} ${
+          completed ? styles.completed : ""
+        }`}
       >
-        <span className={styles.name}>{name}</span>
-      </div>
-      <div className={styles.option_container}>
-        <button
-          type="button"
-          title="Abrir tarefa"
-          className={styles.button}
-          onClick={() => router.push(`/todos/${id}`)}
+        <div
+          className={styles.todo}
+          tabIndex={0}
+          onClick={() => completeTodo(id)}
         >
-          <FiInfo size={22} className={styles.icon} />
-        </button>
-        <button type="button" title="Editar tarefa" className={styles.button}>
-          <FiEdit size={22} className={styles.icon} />
-        </button>
-        <button
-          type="button"
-          title="Remover tarefa"
-          className={styles.button}
-          onClick={() => removeTodo(id)}
-        >
-          <HiOutlineTrash size={22} className={styles.icon} />
-        </button>
+          <span className={styles.name}>{name}</span>
+        </div>
+        <div className={styles.option_container}>
+          <button
+            type="button"
+            title="Abrir tarefa"
+            className={styles.button}
+            onClick={() => router.push(`/todos/${id}`)}
+          >
+            <FiInfo size={22} className={styles.icon} />
+          </button>
+          <button
+            type="button"
+            title="Editar tarefa"
+            className={styles.button}
+            onClick={() => {
+              setTodoId(id);
+
+              handleOpenModal();
+            }}
+          >
+            <FiEdit size={22} className={styles.icon} />
+          </button>
+          <button
+            type="button"
+            title="Remover tarefa"
+            className={styles.button}
+            onClick={() => removeTodo(id)}
+          >
+            <HiOutlineTrash size={22} className={styles.icon} />
+          </button>
+        </div>
       </div>
-    </div>
+      <TodoModal />
+    </>
   );
 };
